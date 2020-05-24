b85b7a7784f526ce604c4b1240a045e0d8f3afa0
Adele Gryba <adelgryb243@ed.amdsb.ca>
2020-05-23 22:21:02 -0400

    Add project files.

---
+++ lottery/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lotteryButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lotteryButton
+            // 
+            this.lotteryButton.BackColor = System.Drawing.Color.Maroon;
+            this.lotteryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lotteryButton.Location = new System.Drawing.Point(277, 104);
+            this.lotteryButton.Name = "lotteryButton";
+            this.lotteryButton.Size = new System.Drawing.Size(243, 68);
+            this.lotteryButton.TabIndex = 0;
+            this.lotteryButton.Text = "generate winning numbers!";
+            this.lotteryButton.UseVisualStyleBackColor = false;
+            this.lotteryButton.Click += new System.EventHandler(this.lotteryButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(184, 196);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(439, 202);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.lotteryButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lotteryButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ lottery/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lottery
+{
+    public partial class Form1 : Form
+    {
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        int num5;
+        int num6;
+        public const int MAX = 60;//max value that can be on ticket
+        Random numberMaker = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lotteryButton_Click(object sender, EventArgs e)
+        {
+            num1 = numberMaker.Next(1, MAX);
+            num2 = numberMaker.Next(1, MAX);
+            while (num2 == num1)
+            {
+                num2 = numberMaker.Next(1, MAX);
+            }
+            while (num3 == num2)//
+            {
+                num3 = numberMaker.Next(1, MAX);
+            }
+            while (num4 == num3)
+            {
+                num4 = numberMaker.Next(1, MAX);
+            }
+            while (num5 == num4)//
+            {
+                num5 = numberMaker.Next(1, MAX);
+            }
+            while (num6 == num5)
+            {
+                num6 = numberMaker.Next(1, MAX);
+            }
+            outputLabel.Text = "your numbers are: " + num1 +" "+ num2 + " " + num3 + " " + num4 + " " + num5 + " " + num6;
+        }
+    }
+}
